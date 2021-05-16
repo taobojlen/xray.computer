8f738a1572b85c709b573ffb1b41ece6413de814
Tao Bojlén <2803708-tao_oat@users.noreply.gitlab.com>
2021-05-16 11:27:19 +0000

    setup dokku deploys

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :xray, XrayWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
-  check_origin: [
-    "https://" <> System.get_env("RENDER_EXTERNAL_HOSTNAME"),
-    "https://xray.computer"
+  url: [
+    host: "xray.computer",
+    port: String.to_integer(System.get_env("PORT") || "80")
   ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
