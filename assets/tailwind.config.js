81a4c133887c62a6a1f25c496d47e594f31730ee
Tao Bojlén <2803708-tao_oat@users.noreply.gitlab.com>
2021-05-23 11:51:45 +0100

    fix tailwind purging of surface components

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   purge: [
+    "../lib/xray_web/components/**/*.ex",
+    "../lib/xray_web/components/**/*.sface",
     "../lib/xray_web/live/**/*.ex",
     "../lib/xray_web/live/**/*.leex",
+    "../lib/xray_web/live/**/*.sface",
     "../lib/xray_web/templates/**/*.eex",
     "../lib/xray_web/templates/**/*.leex",
     "../lib/xray_web/views/**/*.ex",
